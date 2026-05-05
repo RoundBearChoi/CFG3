4211181ed4a8ce2bdd8561c3014a70f8d5761438
roundbearchoi <roundbeargames@gmail.com>
2026-05-05 19:28:08 +0900

    save

--- src/rbg_game/rbg_game.c
+++ src/rbg_game/rbg_game.c
@@ -1,7 +1,9 @@
 #include "rbg_game.h"
+#include "rbg_scenes/rbg_scenes.h"
 #include "rbg_render_debug/rbg_render_debug.h"
 
 void rbg_update_game(void)
 {
+	rbg_update_scenes();
 	rbg_render_debug();
 }
