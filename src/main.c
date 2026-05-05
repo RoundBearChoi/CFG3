a6ba61b5881babd2dd581ddacb45fee4974c7fbc
roundbearchoi <roundbeargames@gmail.com>
2026-05-05 11:51:22 +0900

    dark background

--- src/main.c
+++ src/main.c
@@ -23,21 +23,13 @@ int main(void)
         // ── All drawing must happen between BeginDrawing() and EndDrawing()
         BeginDrawing();
 
-        // Clear the screen with a nice light background
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
 		rbg_render_debug();
 
-        // Simple welcome text (centered-ish)
         DrawText("Congrats! You created your first raylib window in C!",
                  120, 180, 20, LIGHTGRAY);
 
-        // ── Example shapes 
-        // DrawRectangle(300, 300, 200, 150, RED);
-        // DrawCircle(600, 300, 60, BLUE);
-        // DrawLine(100, 100, 700, 400, GREEN);
-
-        // Optional: show current FPS in top-left corner
         DrawFPS(10, 10);
 
         EndDrawing();
