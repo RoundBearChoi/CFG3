73c4d166960d231e9cfd3ad3b6b8e06bf495bae8
roundbearchoi <roundbeargames@gmail.com>
2026-05-05 19:30:24 +0900

    save

--- src/rbg_game/rbg_render_debug/rbg_render_debug.h
+++ src/rbg_game/rbg_render_debug/rbg_render_debug.h
@@ -1,11 +1,6 @@
 #ifndef RBG_RENDER_DEBUG_H
 #define RBG_RENDER_DEBUG_H
 
-// ================================================
-// RBG Render Debug System
-// ================================================
-// All debug visualization lives here.
-
 void rbg_render_debug(void);
 
 #endif
