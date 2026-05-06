5eb1efe7689f608b3d43c9a688622ecab0cd5cc5
roundbearchoi <roundbeargames@gmail.com>
2026-05-06 12:24:27 +0900

    static bool

--- src/rbg_game/rbg_cam_control/rbg_cam_control.c
+++ src/rbg_game/rbg_cam_control/rbg_cam_control.c
@@ -4,9 +4,18 @@
 // Definition (no extern) – this is the real variable
 Camera2D rbg_global_camera;
 
+// Internal linkage → other files can't see it
+static bool rbg_cam_initialized = false;
+
 void rbg_update_cam_control(void)
 {
-    rbg_global_camera.offset   = (Vector2){ 100, 100 };
-    rbg_global_camera.rotation = 0.0f;
-    rbg_global_camera.zoom     = 1.0f;
+	// run once
+	if (rbg_cam_initialized == false)
+	{
+		rbg_global_camera.offset   = (Vector2){ 100, 100 };
+		rbg_global_camera.rotation = 0.0f;
+		rbg_global_camera.zoom     = 1.0f;
+
+		rbg_cam_initialized = true;
+	}
 }
