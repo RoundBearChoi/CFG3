21a6dfc1069ff12cdc2e6e82de3a17670020fe2f
roundbearchoi <roundbeargames@gmail.com>
2026-05-10 07:21:21 +0900

    scaling font for screenspace

--- src/rbg_game/rbg_render_on_screenspace.c
+++ src/rbg_game/rbg_render_on_screenspace.c
@@ -5,7 +5,23 @@
 
 void rbg_render_on_screenspace(void)
 {
-	DrawText(TextFormat("fixed update count: %i", rbg_fixed_updates), 10, 10, 20, GREEN);
+	rbg_draw_scaled_text(TextFormat("fixed update count: %i", rbg_fixed_updates), 10, 10, 16, WHITE);
 
-	//DrawText(TextFormat("recording frame: %i", rbg_current_record_frame), 10, 40, 20, GREEN);
+	rbg_draw_scaled_text(TextFormat("recording frame: %i", rbg_current_record_frame), 10, 30, 16, WHITE);
+}
+
+void rbg_draw_scaled_text(const char* text, int baseX, int baseY, int fontSize, Color color)
+{
+    // Current render dimensions (updates automatically on resize/fullscreen)
+    int screenHeight = GetScreenHeight();
+
+    // Scale everything relative to 720p reference
+    float scale = (float)screenHeight / 720.0f;
+
+    fontSize = (int)(fontSize * scale + 0.5f);   // matches original font size
+
+    int x = (int)(baseX * scale + 0.5f);
+    int y = (int)(baseY * scale + 0.5f);
+
+    DrawText(text, x, y, fontSize, color);
 }
--- src/rbg_game/rbg_render_on_screenspace.h
+++ src/rbg_game/rbg_render_on_screenspace.h
@@ -1,6 +1,9 @@
 #ifndef RBG_RENDER_ON_SCREENSPACE_H
 #define RBG_RENDER_ON_SCREENSPACE_H
 
+#include "raylib.h"
+
 void rbg_render_on_screenspace(void);
+void rbg_draw_scaled_text(const char* text, int baseX, int baseY, int fontSize, Color color);
 
 #endif
