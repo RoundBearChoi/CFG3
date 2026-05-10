7fffbceb4f11ec916a25f50d8d7cc90fd27f3508
roundbearchoi <roundbeargames@gmail.com>
2026-05-10 11:45:08 +0900

    fine tune font

--- src/rbg_game/rbg_render_on_screenspace.c
+++ src/rbg_game/rbg_render_on_screenspace.c
@@ -8,25 +8,25 @@ extern Font press_start;
 
 void rbg_render_on_screenspace(void)
 {
-	rbg_draw_scaled_text(TextFormat("FIXED UPDATE COUNT: %i", rbg_fixed_updates), (Vector2){ 10.0f, 10.0f }, 14.0f, WHITE);
+	// scale 720p reference
+    int screenHeight = GetScreenHeight();
+    float scale = (float)screenHeight / 720.0f;
+	float fontSize = 16.0f * scale;
 
-	rbg_draw_scaled_text(TextFormat("RECORDING FRAME: %i", rbg_current_record_frame), (Vector2) { 10.0f, 50.0f }, 14.0f, WHITE);
+	float horizontal_gap = 20.0f * scale;
+	float font_spacing = 3.0f * scale;
+
+	rbg_draw_scaled_text(TextFormat("FIXED UPDATE COUNT: %i", rbg_fixed_updates), (Vector2){ 10.0f, 15.0f }, fontSize, font_spacing, WHITE);
+
+	rbg_draw_scaled_text(TextFormat("RECORDING FRAME: %i", rbg_current_record_frame), (Vector2){ 10.0f, 15.0f + horizontal_gap }, fontSize, font_spacing, WHITE);
 }
 
-void rbg_draw_scaled_text(const char* text, Vector2 pos, float fontSize, Color color)
+void rbg_draw_scaled_text(const char* text, Vector2 pos, float fontSize, float font_spacing, Color color)
 {
-    // Current render dimensions (updates automatically on resize/fullscreen)
-    int screenHeight = GetScreenHeight();
-
-    // Scale everything relative to 720p reference
-    float scale = (float)screenHeight / 720.0f;
-
-    fontSize = fontSize * scale;
-
 	DrawTextEx(press_start,
                text,
                pos, // must be float
                fontSize, // must be float
-               6, // spacing
+               font_spacing, // must be float
                color);
 }
