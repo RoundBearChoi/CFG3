5f67cce78a808f79bdee37bc328be014b79d655a
roundbearchoi <roundbeargames@gmail.com>
2026-05-10 10:34:00 +0900

    sprite position on player

--- src/rbg_game/rbg_scenes/rbg_players/rbg_players.c
+++ src/rbg_game/rbg_scenes/rbg_players/rbg_players.c
@@ -10,10 +10,10 @@ void rbg_init_players(void)
 {
 	printf("\n=== initializing players ===\n");
 
-	rbg_player_1.position = (Vector2){ 200.0f, 0.0f };
+	rbg_player_1.position = (Vector2){ 50.0f, 0.0f };
 	rbg_player_1.is_facing_right_side = true;
 	
-	rbg_player_2.position = (Vector2){ 600.0f, 0.0f };
+	rbg_player_2.position = (Vector2){ 200.0f, 0.0f };
 	rbg_player_2.is_facing_right_side = false;
 }
 
--- src/rbg_game/rbg_scenes/test_scene_2.c
+++ src/rbg_game/rbg_scenes/test_scene_2.c
@@ -26,7 +26,7 @@ void init_test_scene_2(void)
 	rbg_init_players();
 
 	// temp - update can get info from player. probably won't need separate init
-	rbg_init_sprite_sheet(&sheetPlayer, "fighter_0_idle", RENDER_PIVOT_CENTER);
+	rbg_init_sprite_sheet(&sheetPlayer, "fighter_0_idle", RENDER_PIVOT_BOTTOM_CENTER);
 	rbg_init_sprite_sheet(&sheetPlayer_2, "fighter_0_idle", RENDER_PIVOT_BOTTOM_CENTER);
 
 	// temp testing
@@ -61,8 +61,8 @@ void update_test_scene_2(void)
 	rbg_update_sprite_sheet(&sheetPlayer);
 	rbg_update_sprite_sheet(&sheetPlayer_2);
 
-	rbg_draw_sprite_sheet(&sheetPlayer, (Vector2){ 500.0f , 0.0f }, 1.0f, WHITE);
-	rbg_draw_sprite_sheet(&sheetPlayer_2, (Vector2){ 800.0f, 0.0f }, 1.0f, GRAY);
+	rbg_draw_sprite_sheet(&sheetPlayer, rbg_player_1.position, 1.0f, WHITE);
+	rbg_draw_sprite_sheet(&sheetPlayer_2, (Vector2){ 400.0f, 0.0f }, 1.0f, GRAY);
 
 	if (IsInputActionPressed(INPUT_F1))
 	{
