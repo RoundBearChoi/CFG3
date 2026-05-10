00acc3fa1541ce33b764e588ef36791a3eeedff7
roundbearchoi <roundbeargames@gmail.com>
2026-05-10 11:47:29 +0900

    save

--- src/rbg_game/rbg_render_on_screenspace.c
+++ src/rbg_game/rbg_render_on_screenspace.c
@@ -11,22 +11,21 @@ void rbg_render_on_screenspace(void)
 	// scale 720p reference
     int screenHeight = GetScreenHeight();
     float scale = (float)screenHeight / 720.0f;
-	float fontSize = 16.0f * scale;
-
-	float horizontal_gap = 20.0f * scale;
+	float font_size = 16.0f * scale;
 	float font_spacing = 3.0f * scale;
+	float horizontal_gap = 20.0f * scale;
 
-	rbg_draw_scaled_text(TextFormat("FIXED UPDATE COUNT: %i", rbg_fixed_updates), (Vector2){ 10.0f, 15.0f }, fontSize, font_spacing, WHITE);
+	rbg_draw_scaled_text(TextFormat("FIXED UPDATE COUNT: %i", rbg_fixed_updates), (Vector2){ 10.0f, 15.0f }, font_size, font_spacing, WHITE);
 
-	rbg_draw_scaled_text(TextFormat("RECORDING FRAME: %i", rbg_current_record_frame), (Vector2){ 10.0f, 15.0f + horizontal_gap }, fontSize, font_spacing, WHITE);
+	rbg_draw_scaled_text(TextFormat("RECORDING FRAME: %i", rbg_current_record_frame), (Vector2){ 10.0f, 15.0f + horizontal_gap }, font_size, font_spacing, WHITE);
 }
 
-void rbg_draw_scaled_text(const char* text, Vector2 pos, float fontSize, float font_spacing, Color color)
+void rbg_draw_scaled_text(const char* text, Vector2 pos, float font_size, float font_spacing, Color color)
 {
 	DrawTextEx(press_start,
                text,
                pos, // must be float
-               fontSize, // must be float
+               font_size, // must be float
                font_spacing, // must be float
                color);
 }
