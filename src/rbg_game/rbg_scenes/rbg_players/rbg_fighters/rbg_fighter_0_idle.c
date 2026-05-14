6500012c56e37ee22f166c00c8555653d6c0d1cc
roundbearchoi <roundbeargames@gmail.com>
2026-05-14 17:27:45 +0900

    enum to string test

--- src/rbg_game/rbg_scenes/rbg_players/rbg_fighters/rbg_fighter_0_idle.c
+++ src/rbg_game/rbg_scenes/rbg_players/rbg_fighters/rbg_fighter_0_idle.c
@@ -6,5 +6,7 @@ void update_fighter_0_idle(rbg_player* player)
 	if (new_state_detected(player))
 	{
 		printf("player %d switched to idle state\n", player->player_index);
+
+		printf("enum to string literal: %s\n", rbg_fighter_state_strings[fighter_0_idle]);
 	}
 }
--- src/rbg_game/rbg_scenes/rbg_players/rbg_players.c
+++ src/rbg_game/rbg_scenes/rbg_players/rbg_players.c
@@ -6,7 +6,7 @@ rbg_player rbg_player_1;
 rbg_player rbg_player_2;
 
 // X-macro generated string literals array (must come after the header definition)
-const char* const rbg_fighter_curr_state_strings[NUM_FIGHTER_STATES] = {
+const char* const rbg_fighter_state_strings[NUM_FIGHTER_STATES] = {
 #define X(state) [state] = #state,
     RBG_FIGHTER_STATES(X)
 #undef X
