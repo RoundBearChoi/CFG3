5b258bf711db1241c80e8c2b226cdde8f0d14e64
roundbearchoi <roundbeargames@gmail.com>
2026-05-24 22:06:34 +0900

    save

--- src/rbg_game/rbg_scenes/rbg_players/rbg_basic_collision_resolve.c
+++ src/rbg_game/rbg_scenes/rbg_players/rbg_basic_collision_resolve.c
@@ -1,6 +1,15 @@
 #include "rbg_basic_collision_resolve.h"
+#include "rbg_player_boxes.h"
 
 void rbg_update_basic_collision_resolve(void)
 {
+	if (rbg_player_1_box.collision_with_other_player == true)
+	{
 
+	}
+
+	if (rbg_player_2_box.collision_with_other_player == true)
+	{
+
+	}
 }
