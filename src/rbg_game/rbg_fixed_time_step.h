7777e2644d94b6d060119266d8712c036290f461
roundbearchoi <roundbeargames@gmail.com>
2026-07-08 20:22:32 +0900

    save

--- src/rbg_game/rbg_fixed_time_step.h
+++ src/rbg_game/rbg_fixed_time_step.h
@@ -1,8 +1,8 @@
 #ifndef RBG_FIXED_TIME_STEP_H
 #define RBG_FIXED_TIME_STEP_H
 
+#include "rbg_game.h"
 #include <stdbool.h>
-#include "rbg_game.h"   // for RbgGameContext
 
 extern double global_rbg_frame_time;
 extern const int global_rbg_target_fps;
