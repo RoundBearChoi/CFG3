5bba53454ba713a4963b37164b8181bc90d14ba6
roundbearchoi <roundbeargames@gmail.com>
2026-07-10 14:35:02 +0900

    save

--- src/rbg_game/rbg_players/rbg_fighters/rbg_fighter_updater.c
+++ src/rbg_game/rbg_players/rbg_fighters/rbg_fighter_updater.c
@@ -15,11 +15,11 @@ rbg_fighter_update_func rbg_fighter_update_functions[NUM_FIGHTER_STATES];
 
 void rbg_init_fighter_update_functions(void)
 {
-    rbg_fighter_update_functions[fighter_uninitialized] = update_fighter_uninitialized;
-    rbg_fighter_update_functions[fighter_0_idle]       = update_fighter_0_idle;
-    rbg_fighter_update_functions[fighter_0_walk]       = update_fighter_0_walk;
-    rbg_fighter_update_functions[fighter_0_jump]       = update_fighter_0_jump;
-    rbg_fighter_update_functions[fighter_0_fall]       = update_fighter_0_fall;
+    rbg_fighter_update_functions[fighter_uninitialized]   = update_fighter_uninitialized;
+    rbg_fighter_update_functions[fighter_0_idle]          = update_fighter_0_idle;
+    rbg_fighter_update_functions[fighter_0_walk]          = update_fighter_0_walk;
+    rbg_fighter_update_functions[fighter_0_jump]          = update_fighter_0_jump;
+    rbg_fighter_update_functions[fighter_0_fall]          = update_fighter_0_fall;
 }
 
 void rbg_update_fighter(RbgGameContext* game_ctx, rbg_player* player)
