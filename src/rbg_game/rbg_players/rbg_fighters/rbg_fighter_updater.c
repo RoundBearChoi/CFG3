fae2a3927ea458f1b4787de2c132f1de7d6f37e9
roundbearchoi <roundbeargames@gmail.com>
2026-07-12 22:01:29 +0900

    script order

--- src/rbg_game/rbg_players/rbg_fighters/rbg_fighter_updater.c
+++ src/rbg_game/rbg_players/rbg_fighters/rbg_fighter_updater.c
@@ -17,10 +17,10 @@ rbg_fighter_update_func rbg_fighter_update_functions[NUM_FIGHTER_STATES];
 void rbg_init_fighter_update_functions(void)
 {
     rbg_fighter_update_functions[fighter_uninitialized]   = update_fighter_uninitialized;
-    rbg_fighter_update_functions[fighter_0_idle]          = update_fighter_0_idle;
-    rbg_fighter_update_functions[fighter_0_walk]          = update_fighter_0_walk;
-    rbg_fighter_update_functions[fighter_0_jump]          = update_fighter_0_jump;
     rbg_fighter_update_functions[fighter_0_fall]          = update_fighter_0_fall;
+    rbg_fighter_update_functions[fighter_0_idle]          = update_fighter_0_idle;
+    rbg_fighter_update_functions[fighter_0_jump]          = update_fighter_0_jump;
+    rbg_fighter_update_functions[fighter_0_walk]          = update_fighter_0_walk;
 }
 
 void rbg_update_fighter(RbgGameContext* game_ctx, int player_index)
