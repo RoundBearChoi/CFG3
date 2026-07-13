1e341b90324000c8868ed021d8aebe53454c9ff7
RoundBearChoi <roundbeargames@gmail.com>
2026-07-13 12:36:03 +0900

    moving player stuff to player ctx

--- src/rbg_game/rbg_game_context.h
+++ src/rbg_game/rbg_game_context.h
@@ -1,6 +1,7 @@
 #ifndef RBG_GAME_CONTEXT_H
 #define RBG_GAME_CONTEXT_H
 
+#include "rbg_player_context.h"
 #include <stdbool.h>
 #include "raylib.h"
 
@@ -47,44 +48,6 @@ typedef enum rbg_scene_type {
     TEST_SCENE_2,
     NUM_SCENES
 } rbg_scene_type;
-
-typedef enum rbg_fighter_state {
-	fighter_0_none,
-	fighter_0_idle,
-    fighter_0_walk,
-    fighter_0_jump,
-    fighter_0_fall,
-    NUM_FIGHTER_STATES,
-} rbg_fighter_state;
-
-// Enum that controls where the 'position' parameter points to in rbg_draw_sprite_animation.
-// CENTER is the original behavior; BOTTOM_CENTER is useful for characters standing on the ground.
-typedef enum rbg_render_pivot {
-    RENDER_PIVOT_CENTER,
-    RENDER_PIVOT_BOTTOM_CENTER
-} rbg_render_pivot;
-
-typedef struct rbg_player {
-	int player_index;
-    Vector2 position;
-    bool is_facing_right_side;
-	rbg_fighter_state fighter_curr_state;
-	rbg_fighter_state fighter_prev_state;
-} rbg_player;
-
-typedef struct rbg_player_col_box {
-	int player_index;
-	bool is_colliding_with_other_player;
-
-	float width;
-	float height;
-	float offset_x;
-	float offset_y;
-
-	// Collision result stored after detection (to avoid recalculating in resolve)
-	float overlap_x;   // Positive value = how much the boxes overlap horizontally
-
-} rbg_player_col_box;
 
 typedef struct SpriteSheet {
     char *fighter_type;
---
+++ src/rbg_game/rbg_player_context.h
@@ -0,0 +1,44 @@
+#ifndef RBG_PLAYER_CONTEXT_H
+#define RBG_PLAYER_CONTEXT_H
+
+#include "raylib.h"
+
+typedef enum rbg_fighter_state {
+	fighter_0_none,
+	fighter_0_idle,
+    fighter_0_walk,
+    fighter_0_jump,
+    fighter_0_fall,
+    NUM_FIGHTER_STATES,
+} rbg_fighter_state;
+
+// Enum that controls where the 'position' parameter points to in rbg_draw_sprite_animation.
+// CENTER is the original behavior; BOTTOM_CENTER is useful for characters standing on the ground.
+typedef enum rbg_render_pivot {
+    RENDER_PIVOT_CENTER,
+    RENDER_PIVOT_BOTTOM_CENTER
+} rbg_render_pivot;
+
+typedef struct rbg_player {
+	int player_index;
+    Vector2 position;
+    bool is_facing_right_side;
+	rbg_fighter_state fighter_curr_state;
+	rbg_fighter_state fighter_prev_state;
+} rbg_player;
+
+typedef struct rbg_player_col_box {
+	int player_index;
+	bool is_colliding_with_other_player;
+
+	float width;
+	float height;
+	float offset_x;
+	float offset_y;
+
+	// Collision result stored after detection (to avoid recalculating in resolve)
+	float overlap_x;   // Positive value = how much the boxes overlap horizontally
+
+} rbg_player_col_box;
+
+#endif
