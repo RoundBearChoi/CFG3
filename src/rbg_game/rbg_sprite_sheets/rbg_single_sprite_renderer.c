f4905e1d508fd584db3e9563f64a60998ad33e7b
roundbearchoi <roundbeargames@gmail.com>
2026-07-18 13:46:18 +0900

    passing tint

--- src/rbg_game/rbg_sprite_sheets/rbg_single_sprite_renderer.c
+++ src/rbg_game/rbg_sprite_sheets/rbg_single_sprite_renderer.c
@@ -1,12 +1,12 @@
 #include "rbg_single_sprite_renderer.h"
 #include "../rbg_sprite_sheets/rbg_single_sprite_loader.h"
 
-void rbg_render_single_sprite(RbgGameContext* game_ctx, const char *sprite_name, Vector2 position, float scale)
+void rbg_render_single_sprite(RbgGameContext* game_ctx, const char *sprite_name, Vector2 position, float scale, Color tint)
 {
     single_sprite* sprite = rbg_get_single_sprite_by_name(game_ctx, sprite_name);
     
 	if (sprite && sprite->texture.id != 0)
     {
-        DrawTextureEx(sprite->texture, position, 0.0f, scale, WHITE);
+        DrawTextureEx(sprite->texture, position, 0.0f, scale, tint);
     }
 }
--- src/rbg_game/rbg_sprite_sheets/rbg_single_sprite_renderer.h
+++ src/rbg_game/rbg_sprite_sheets/rbg_single_sprite_renderer.h
@@ -3,6 +3,6 @@
 
 #include "../rbg_game_context.h"
 
-void rbg_render_single_sprite(RbgGameContext* game_ctx, const char *sprite_name, Vector2 position, float scale);
+void rbg_render_single_sprite(RbgGameContext* game_ctx, const char *sprite_name, Vector2 position, float scale, Color tint);
 
 #endif
