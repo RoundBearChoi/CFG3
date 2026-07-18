5b792a6597af8e3ebe13b7bccd99d9d545d20b0e
roundbearchoi <roundbeargames@gmail.com>
2026-07-18 13:39:45 +0900

    cat test func

---
+++ src/rbg_game/rbg_sprite_sheets/rbg_single_sprite_renderer.h
@@ -0,0 +1,8 @@
+#ifndef RBG_SINGLE_SPRITE_RENDERER_H
+#define RBG_SINGLE_SPRITE_RENDERER_H
+
+#include "../rbg_game_context.h"
+
+void rbg_render_single_sprite(RbgGameContext* game_ctx, const char *sprite_name, Vector2 position, float scale);
+
+#endif
