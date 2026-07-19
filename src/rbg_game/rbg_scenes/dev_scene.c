ae3ae051a84cbfa48cf68238e7adc3cc9beff5f7
roundbearchoi <roundbeargames@gmail.com>
2026-07-19 11:31:59 +0900

    save

--- src/rbg_game/rbg_scenes/dev_scene.c
+++ src/rbg_game/rbg_scenes/dev_scene.c
@@ -8,11 +8,15 @@ void init_dev_scene(RbgGameContext* game_ctx)
 	printf("\n=== starting dev scene ===\n");
 
 	game_ctx->scene_initialized = true;
+
+	rbg_load_single_sprites(game_ctx, "resources/single_sprite_list.csv");
 }
 
 void end_dev_scene(RbgGameContext* game_ctx)
 {
 	game_ctx->scene_initialized = false;
+
+	rbg_unload_single_sprites(game_ctx);
 }
 
 void update_dev_scene(RbgGameContext* game_ctx)
@@ -25,5 +29,5 @@ void update_dev_scene(RbgGameContext* game_ctx)
 
 void render_dev_scene(RbgGameContext* game_ctx)
 {
-	rbg_render_single_sprite(game_ctx, "retro_cat_test", (Vector2){ 0.0f, -500.0f }, 3.0f, RENDER_PIVOT_BOTTOM_CENTER, GRAY);
+	rbg_render_single_sprite(game_ctx, "retro_cat_test", (Vector2){ 0.0f, 0.0f }, 3.0f, RENDER_PIVOT_BOTTOM_CENTER, GRAY);
 }
